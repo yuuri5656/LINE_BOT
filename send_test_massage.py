52efdca217226f9890c4f70f4d42719227696288
yuuri5656 <109333851+yuuri5656@users.noreply.github.com>
2025-10-30 16:36:17 +0900

    Update access token and group ID in send_test_massage.py

--- send_test_massage.py
+++ send_test_massage.py
@@ -1,8 +1,8 @@
 import requests
 
 # ---- 設定 ----
-CHANNEL_ACCESS_TOKEN = "ここにチャネルアクセストークン"
-GROUP_ID = "ここにグループID"
+CHANNEL_ACCESS_TOKEN = "s1mjVUw5pRMoGE5GeVaacpYiEJgj561EbudGSQlm17JahyLIlVpnZi/GchGsp0XSzJE33BLGOiYAHXTJL9Ryk5aLU28zt2mRCC7cOOGOljfLIJttB2AD6ut4Et1I1HWubhI5/XHBkwtcV+Hy7OOKmwdB04t89/1O/w1cDnyilFU="
+GROUP_ID = "C76594d544af27b7c51f332f341b345b4"
 
 # ---- メッセージ ----
 message = "こんにちは！これはテストメッセージです。"
